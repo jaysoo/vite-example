1506e0a7414b5261855dc5704c724cad40891d0b
Jack Hsu <jack.hsu@gmail.com>
2024-06-25 13:56:42 -0400

    turn off timeout

--- packages/dashboard-widgets/vitest.config.ts
+++ packages/dashboard-widgets/vitest.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    isolate: false,
     pool: 'forks',
-    poolOptions: {
-      forks: {
-        maxForks: 1,
-      },
-    },
     environment: 'jsdom',
     watch: false,
   },
